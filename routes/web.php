5f6519167a9ac9477763114c051abf55cf7110d8
= evi03 <= evi.nzbk@gmail.com>
2023-02-23 12:01:00 +0700

    02_praktikum_web_lanjut_satu

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function() {
+    echo "Selamat Datang";
+    });
+
+Route::get('/about', function() {
+    echo "2141720030 Evi amalia Midfia";
+    });
+
+Route::get('/about/{id}', function($id) {
+    echo "NIM: 2141720030, Nama: Evi amalia Midfia Dengan Id $id";
+    });
+
+Route::get('/articles/{id}', function($id) {
+    echo "Halaman artikel dengan id $id";
+    });
